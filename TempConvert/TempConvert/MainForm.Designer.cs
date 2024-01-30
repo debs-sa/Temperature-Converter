03dfe7e58532c67132d40ef572315656f6636a0d
debs-sa <debs.saas@gmail.com>
2024-01-30 16:24:12 -0700

    project upload

---
+++ TempConvert/TempConvert/MainForm.Designer.cs
@@ -0,0 +1,346 @@
+﻿
+namespace TempConvert
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTwo = new System.Windows.Forms.Button();
+            this.btnSix = new System.Windows.Forms.Button();
+            this.btnFive = new System.Windows.Forms.Button();
+            this.btnEight = new System.Windows.Forms.Button();
+            this.btnOne = new System.Windows.Forms.Button();
+            this.btnSeven = new System.Windows.Forms.Button();
+            this.btnPlusMin = new System.Windows.Forms.Button();
+            this.btnZero = new System.Windows.Forms.Button();
+            this.btnNine = new System.Windows.Forms.Button();
+            this.btnPoint = new System.Windows.Forms.Button();
+            this.btnThree = new System.Windows.Forms.Button();
+            this.btnFour = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.lblTemprature = new System.Windows.Forms.Label();
+            this.lblConvert = new System.Windows.Forms.Label();
+            this.lblInformation = new System.Windows.Forms.Label();
+            this.lblDiffer = new System.Windows.Forms.Label();
+            this.cmbTemperature = new System.Windows.Forms.ComboBox();
+            this.cmbConverter = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnTwo
+            // 
+            this.btnTwo.ForeColor = System.Drawing.Color.Purple;
+            this.btnTwo.Location = new System.Drawing.Point(167, 301);
+            this.btnTwo.Name = "btnTwo";
+            this.btnTwo.Size = new System.Drawing.Size(130, 110);
+            this.btnTwo.TabIndex = 0;
+            this.btnTwo.Text = "2";
+            this.btnTwo.UseVisualStyleBackColor = true;
+            this.btnTwo.Click += new System.EventHandler(this.Number_Temperature);
+            // 
+            // btnSix
+            // 
+            this.btnSix.ForeColor = System.Drawing.Color.Purple;
+            this.btnSix.Location = new System.Drawing.Point(303, 185);
+            this.btnSix.Name = "btnSix";
+            this.btnSix.Size = new System.Drawing.Size(130, 110);
+            this.btnSix.TabIndex = 1;
+            this.btnSix.Text = "6";
+            this.btnSix.UseVisualStyleBackColor = true;
+            this.btnSix.Click += new System.EventHandler(this.Number_Temperature);
+            // 
+            // btnFive
+            // 
+            this.btnFive.ForeColor = System.Drawing.Color.Purple;
+            this.btnFive.Location = new System.Drawing.Point(167, 185);
+            this.btnFive.Name = "btnFive";
+            this.btnFive.Size = new System.Drawing.Size(130, 110);
+            this.btnFive.TabIndex = 2;
+            this.btnFive.Text = "5";
+            this.btnFive.UseVisualStyleBackColor = true;
+            this.btnFive.Click += new System.EventHandler(this.Number_Temperature);
+            // 
+            // btnEight
+            // 
+            this.btnEight.ForeColor = System.Drawing.Color.Purple;
+            this.btnEight.Location = new System.Drawing.Point(167, 69);
+            this.btnEight.Name = "btnEight";
+            this.btnEight.Size = new System.Drawing.Size(130, 110);
+            this.btnEight.TabIndex = 3;
+            this.btnEight.Text = "8";
+            this.btnEight.UseVisualStyleBackColor = true;
+            this.btnEight.Click += new System.EventHandler(this.Number_Temperature);
+            // 
+            // btnOne
+            // 
+            this.btnOne.ForeColor = System.Drawing.Color.Purple;
+            this.btnOne.Location = new System.Drawing.Point(31, 301);
+            this.btnOne.Name = "btnOne";
+            this.btnOne.Size = new System.Drawing.Size(130, 110);
+            this.btnOne.TabIndex = 4;
+            this.btnOne.Text = "1";
+            this.btnOne.UseVisualStyleBackColor = true;
+            this.btnOne.Click += new System.EventHandler(this.Number_Temperature);
+            // 
+            // btnSeven
+            // 
+            this.btnSeven.ForeColor = System.Drawing.Color.Purple;
+            this.btnSeven.Location = new System.Drawing.Point(31, 69);
+            this.btnSeven.Name = "btnSeven";
+            this.btnSeven.Size = new System.Drawing.Size(130, 110);
+            this.btnSeven.TabIndex = 5;
+            this.btnSeven.Text = "7";
+            this.btnSeven.UseVisualStyleBackColor = true;
+            this.btnSeven.Click += new System.EventHandler(this.Number_Temperature);
+            // 
+            // btnPlusMin
+            // 
+            this.btnPlusMin.ForeColor = System.Drawing.Color.Green;
+            this.btnPlusMin.Location = new System.Drawing.Point(31, 417);
+            this.btnPlusMin.Name = "btnPlusMin";
+            this.btnPlusMin.Size = new System.Drawing.Size(130, 110);
+            this.btnPlusMin.TabIndex = 6;
+            this.btnPlusMin.Text = "+/-";
+            this.btnPlusMin.UseVisualStyleBackColor = true;
+            this.btnPlusMin.Click += new System.EventHandler(this.btnPlusMin_Click);
+            // 
+            // btnZero
+            // 
+            this.btnZero.ForeColor = System.Drawing.Color.Purple;
+            this.btnZero.Location = new System.Drawing.Point(167, 417);
+            this.btnZero.Name = "btnZero";
+            this.btnZero.Size = new System.Drawing.Size(130, 110);
+            this.btnZero.TabIndex = 7;
+            this.btnZero.Text = "0";
+            this.btnZero.UseVisualStyleBackColor = true;
+            this.btnZero.Click += new System.EventHandler(this.Number_Temperature);
+            // 
+            // btnNine
+            // 
+            this.btnNine.ForeColor = System.Drawing.Color.Purple;
+            this.btnNine.Location = new System.Drawing.Point(303, 69);
+            this.btnNine.Name = "btnNine";
+            this.btnNine.Size = new System.Drawing.Size(130, 110);
+            this.btnNine.TabIndex = 8;
+            this.btnNine.Text = "9";
+            this.btnNine.UseVisualStyleBackColor = true;
+            this.btnNine.Click += new System.EventHandler(this.Number_Temperature);
+            // 
+            // btnPoint
+            // 
+            this.btnPoint.ForeColor = System.Drawing.Color.Green;
+            this.btnPoint.Location = new System.Drawing.Point(303, 417);
+            this.btnPoint.Name = "btnPoint";
+            this.btnPoint.Size = new System.Drawing.Size(130, 110);
+            this.btnPoint.TabIndex = 9;
+            this.btnPoint.Text = ".";
+            this.btnPoint.UseVisualStyleBackColor = true;
+            this.btnPoint.Click += new System.EventHandler(this.Number_Temperature);
+            // 
+            // btnThree
+            // 
+            this.btnThree.ForeColor = System.Drawing.Color.Purple;
+            this.btnThree.Location = new System.Drawing.Point(303, 301);
+            this.btnThree.Name = "btnThree";
+            this.btnThree.Size = new System.Drawing.Size(130, 110);
+            this.btnThree.TabIndex = 10;
+            this.btnThree.Text = "3";
+            this.btnThree.UseVisualStyleBackColor = true;
+            this.btnThree.Click += new System.EventHandler(this.Number_Temperature);
+            // 
+            // btnFour
+            // 
+            this.btnFour.ForeColor = System.Drawing.Color.Purple;
+            this.btnFour.Location = new System.Drawing.Point(31, 185);
+            this.btnFour.Name = "btnFour";
+            this.btnFour.Size = new System.Drawing.Size(130, 110);
+            this.btnFour.TabIndex = 11;
+            this.btnFour.Text = "4";
+            this.btnFour.UseVisualStyleBackColor = true;
+            this.btnFour.Click += new System.EventHandler(this.Number_Temperature);
+            // 
+            // btnClear
+            // 
+            this.btnClear.ForeColor = System.Drawing.Color.Red;
+            this.btnClear.Location = new System.Drawing.Point(31, 533);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(194, 110);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.ForeColor = System.Drawing.Color.Red;
+            this.btnDel.Location = new System.Drawing.Point(231, 533);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(202, 110);
+            this.btnDel.TabIndex = 13;
+            this.btnDel.Text = "Del";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("MV Boli", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.ForeColor = System.Drawing.Color.Yellow;
+            this.lblInfo.Location = new System.Drawing.Point(294, 9);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(451, 49);
+            this.lblInfo.TabIndex = 18;
+            this.lblInfo.Text = "Temperature Converter";
+            // 
+            // lblTemprature
+            // 
+            this.lblTemprature.AutoSize = true;
+            this.lblTemprature.Font = new System.Drawing.Font("MV Boli", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTemprature.ForeColor = System.Drawing.Color.Black;
+            this.lblTemprature.Location = new System.Drawing.Point(480, 110);
+            this.lblTemprature.Name = "lblTemprature";
+            this.lblTemprature.Size = new System.Drawing.Size(49, 49);
+            this.lblTemprature.TabIndex = 19;
+            this.lblTemprature.Text = "0";
+            // 
+            // lblConvert
+            // 
+            this.lblConvert.AutoSize = true;
+            this.lblConvert.BackColor = System.Drawing.Color.Transparent;
+            this.lblConvert.Font = new System.Drawing.Font("MV Boli", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConvert.ForeColor = System.Drawing.Color.Black;
+            this.lblConvert.Location = new System.Drawing.Point(480, 301);
+            this.lblConvert.Name = "lblConvert";
+            this.lblConvert.Size = new System.Drawing.Size(242, 49);
+            this.lblConvert.TabIndex = 20;
+            this.lblConvert.Text = "labelConvert";
+            // 
+            // lblInformation
+            // 
+            this.lblInformation.AutoSize = true;
+            this.lblInformation.Font = new System.Drawing.Font("MV Boli", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInformation.ForeColor = System.Drawing.Color.Black;
+            this.lblInformation.Location = new System.Drawing.Point(480, 517);
+            this.lblInformation.Name = "lblInformation";
+            this.lblInformation.Size = new System.Drawing.Size(319, 49);
+            this.lblInformation.TabIndex = 21;
+            this.lblInformation.Text = "About equal to:";
+            // 
+            // lblDiffer
+            // 
+            this.lblDiffer.AutoSize = true;
+            this.lblDiffer.Font = new System.Drawing.Font("MV Boli", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiffer.ForeColor = System.Drawing.Color.Black;
+            this.lblDiffer.Location = new System.Drawing.Point(480, 580);
+            this.lblDiffer.Name = "lblDiffer";
+            this.lblDiffer.Size = new System.Drawing.Size(206, 49);
+            this.lblDiffer.TabIndex = 22;
+            this.lblDiffer.Text = "labelDiffer";
+            // 
+            // cmbTemperature
+            // 
+            this.cmbTemperature.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.cmbTemperature.FormattingEnabled = true;
+            this.cmbTemperature.Location = new System.Drawing.Point(470, 185);
+            this.cmbTemperature.Name = "cmbTemperature";
+            this.cmbTemperature.Size = new System.Drawing.Size(392, 42);
+            this.cmbTemperature.TabIndex = 23;
+            // 
+            // cmbConverter
+            // 
+            this.cmbConverter.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.cmbConverter.FormattingEnabled = true;
+            this.cmbConverter.Location = new System.Drawing.Point(470, 386);
+            this.cmbConverter.Name = "cmbConverter";
+            this.cmbConverter.Size = new System.Drawing.Size(392, 42);
+            this.cmbConverter.TabIndex = 24;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 34F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(953, 688);
+            this.Controls.Add(this.cmbConverter);
+            this.Controls.Add(this.cmbTemperature);
+            this.Controls.Add(this.lblDiffer);
+            this.Controls.Add(this.lblInformation);
+            this.Controls.Add(this.lblConvert);
+            this.Controls.Add(this.lblTemprature);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnFour);
+            this.Controls.Add(this.btnThree);
+            this.Controls.Add(this.btnPoint);
+            this.Controls.Add(this.btnNine);
+            this.Controls.Add(this.btnZero);
+            this.Controls.Add(this.btnPlusMin);
+            this.Controls.Add(this.btnSeven);
+            this.Controls.Add(this.btnOne);
+            this.Controls.Add(this.btnEight);
+            this.Controls.Add(this.btnFive);
+            this.Controls.Add(this.btnSix);
+            this.Controls.Add(this.btnTwo);
+            this.Font = new System.Drawing.Font("MV Boli", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "MainForm";
+            this.Text = "Temperature Converter";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTwo;
+        private System.Windows.Forms.Button btnSix;
+        private System.Windows.Forms.Button btnFive;
+        private System.Windows.Forms.Button btnEight;
+        private System.Windows.Forms.Button btnOne;
+        private System.Windows.Forms.Button btnSeven;
+        private System.Windows.Forms.Button btnPlusMin;
+        private System.Windows.Forms.Button btnZero;
+        private System.Windows.Forms.Button btnNine;
+        private System.Windows.Forms.Button btnPoint;
+        private System.Windows.Forms.Button btnThree;
+        private System.Windows.Forms.Button btnFour;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Label lblTemprature;
+        private System.Windows.Forms.Label lblConvert;
+        private System.Windows.Forms.Label lblInformation;
+        private System.Windows.Forms.Label lblDiffer;
+        private System.Windows.Forms.ComboBox cmbTemperature;
+        private System.Windows.Forms.ComboBox cmbConverter;
+    }
+}
+
---
+++ TempConvert/TempConvert/MainForm.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TempConvert
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Number_Temperature(object sender, EventArgs e)
+        {
+            Button TempratureButton = (Button)sender;
+            if(lblTemprature.Text == "0")
+            {
+                lblTemprature.Text = "";
+            }
+            if(TempratureButton.Text == ".")
+            {
+                if (!lblTemprature.Text.Contains("."))
+                {
+                    lblTemprature.Text = lblTemprature.Text + TempratureButton.Text;
+                }
+            }
+            else
+            {
+                lblTemprature.Text = lblTemprature.Text + TempratureButton.Text;
+            }
+            calculateTemp();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblTemprature.Text = "0";
+            calculateTemp();
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            if(lblTemprature.Text.Length > 0)
+            {
+                lblTemprature.Text = lblTemprature.Text.Remove(lblTemprature.Text.Length - 1, 1);
+
+            }
+            if(lblTemprature.Text == "")
+            {
+                lblTemprature.Text = "0";
+            }
+            calculateTemp();
+        }
+
+        private void btnPlusMin_Click(object sender, EventArgs e)
+        {
+            if (lblTemprature.Text.Contains("-"))
+            {
+                lblTemprature.Text = lblTemprature.Text.Remove(0, 1);  //-25  
+            }
+            else
+            {
+                lblTemprature.Text = "-" + lblTemprature.Text;
+            }
+            calculateTemp();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            cmbTemperature.Text = ("Fahrenheit");
+
+            cmbTemperature.Items.Add("Celsius");
+            cmbTemperature.Items.Add("Fahrenheit");
+            cmbTemperature.Items.Add("Kelvin");
+
+
+            cmbConverter.Text = ("Celsius");
+
+            cmbConverter.Items.Add("Celsius");
+            cmbConverter.Items.Add("Fahrenheit");
+            cmbConverter.Items.Add("Kelvin");
+
+            calculateTemp();
+        }
+
+        private String Converter(Double value, String tempFrom, String tempTo)
+        {
+            try
+            {
+                switch (tempFrom)
+                {
+                case "Celsius":
+                    switch (tempTo)
+                    {
+                        case "Fahrenheit":
+                            return String.Format("{0}\u00B0F", Math.Round(value * 9 / 5 + 32, 2).ToString());
+                        case "Kelvin":
+                            return String.Format("{0}K", Math.Round(value + 273.15, 2).ToString());
+                        case "Celsius":
+                            return String.Format("{0}\u00B0C", Math.Round(value, 2).ToString());
+                    }
+                    break;
+                case "Fahrenheit":
+                    switch (tempTo)
+                    {
+                        case "Celsius":
+                            return String.Format("{0}\u00B0C", Math.Round((value - 32) * 5 / 9, 2).ToString());
+                        case "Kelvin":
+                            return String.Format("{0}K", Math.Round((((value - 32) * 5) / 9) + 273.15, 2), ToString());
+                        case "Fahrenheit":
+                            return String.Format("{0}\u00B0F", Math.Round(value, 2).ToString());
+                    }
+                    break;
+                case "Kelvin":
+                    switch (tempTo)
+                    {
+                        case "Fahrenheit":
+                            return String.Format("{0}\u00B0F", Math.Round((value - 273.15) * 9 / 5 + 32, 2).ToString());
+                        case "Kelvin":
+                            return String.Format("{0}K", Math.Round(value, 2).ToString());
+                        case "Celsius":
+                            return String.Format("{0}\u00B0C", Math.Round(value - 273.15, 2).ToString());
+                      
+                    }
+                    break;
+                        
+            }
+                throw new ArgumentException("Invalid conversion type");
+            }
+            catch (Exception e)
+            {
+                return "Error: " + e;
+            }
+            
+        }
+        private void calculateTemp()
+        {
+            Double value;
+            value = Double.Parse(lblTemprature.Text);
+
+            lblConvert.Text = Converter(value, cmbTemperature.Text, cmbConverter.Text);
+            if(cmbTemperature == cmbConverter)
+            {
+                lblDiffer.Text = cmbTemperature.Text == "Celsius"
+                    ? String.Format("{0} and {1}", Converter(value, cmbTemperature.Text, "Fahrenheit"), Converter(value, cmbTemperature.Text, "Kelvin"))
+                    : cmbTemperature.Text == "Fahrenheit"
+                    ? String.Format("{0} and {1}", Converter(value, cmbTemperature.Text, "Celsius"), Converter(value, cmbTemperature.Text, "Kelvin"))
+                    : String.Format("{0} and {1}", Converter(value, cmbTemperature.Text, "Celsius"), Converter(value, cmbTemperature.Text, "Fahrenheit"));
+            }
+            else if(cmbTemperature.Text == "Celsius")
+            {
+                lblDiffer.Text = cmbConverter.Text == "Fahrenheit"
+                    ? String.Format("{0}", Converter(value, cmbTemperature.Text, "Kelvin"))
+                    : String.Format("{0}", Converter(value, cmbTemperature.Text, "Fahrenheit"));
+            }
+            else if(cmbTemperature.Text == "Fahrenheit")
+            {
+                lblDiffer.Text = cmbConverter.Text == "Celsius"
+                    ? String.Format("{0}", Converter(value, cmbTemperature.Text, "Kelvin"))
+                    : String.Format("{0}", Converter(value, cmbTemperature.Text, "Celsius"));
+            }
+            else
+            {
+                lblDiffer.Text = cmbConverter.Text == "Celsius"
+                    ? String.Format("{0}", Converter(value, cmbTemperature.Text, "Fahrenheit"))
+                    : String.Format("{0}", Converter(value, cmbTemperature.Text, "Celsius"));
+            }
+        }
+    }
+}
